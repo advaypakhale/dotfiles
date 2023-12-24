ca1a8b3e93d61d0f269b8235b77c0d577572ce15
Advay Pakhale <advay.pakhale@gmail.com>
2023-12-24 17:36:53 +0800

    fixed typo in init

--- config.el
+++ config.el
@@ -36,9 +36,9 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-outrun-electric)
-(after! doom-themes
-  (load-theme 'doom-nano-dark t))
+(setq doom-theme 'doom-one)
+;; (after! doom-themes
+;;   (load-theme 'doom-nano-dark t))
 (setq doom-font (font-spec :family "JetBrains Mono" :size 16))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
