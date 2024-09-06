302b52d9add7ded9accf5debf7e5e340ec7b983d
advaypakhale <advay.pakhale@gmail.com>
2024-09-06 14:29:58 +0800

    init

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,100 @@
+return {
+    {
+        "hrsh7th/nvim-cmp",
+        opts = function()
+            local options = require("configs.completion")
+            return options
+        end,
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("configs.treesitter")
+        end,
+    },
+
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("nvchad.configs.lspconfig").defaults()
+            require("configs.lspconfig")
+        end,
+    },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        event = "VeryLazy",
+        dependencies = { "nvim-lspconfig" },
+        config = function()
+            require("configs.mason-lspconfig")
+        end,
+    },
+
+    {
+        "mfussenegger/nvim-lint",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("configs.lint")
+        end,
+    },
+
+    {
+        "rshkarin/mason-nvim-lint",
+        event = "VeryLazy",
+        dependencies = { "nvim-lint" },
+        config = function()
+            require("configs.mason-lint")
+        end,
+    },
+
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre",
+        config = function()
+            require("configs.conform")
+        end,
+    },
+
+    {
+        "zapling/mason-conform.nvim",
+        event = "VeryLazy",
+        dependencies = { "conform.nvim" },
+        config = function()
+            require("configs.mason-conform")
+        end,
+    },
+
+    {
+        "nvim-java/nvim-java",
+        lazy = false,
+        dependencies = {
+            "nvim-java/lua-async-await",
+            "nvim-java/nvim-java-core",
+            "nvim-java/nvim-java-test",
+            "nvim-java/nvim-java-dap",
+            "MunifTanjim/nui.nvim",
+            "neovim/nvim-lspconfig",
+            "mfussenegger/nvim-dap",
+            {
+                "williamboman/mason.nvim",
+                opts = {
+                    registries = {
+                        "github:nvim-java/mason-registry",
+                        "github:mason-org/mason-registry",
+                    },
+                },
+            },
+        },
+        config = function()
+            require("java").setup({})
+            require("lspconfig").jdtls.setup({
+                on_attach = require("nvchad.configs.lspconfig").on_attach,
+                capabilities = require("nvchad.configs.lspconfig").capabilities,
+                filetypes = { "java" },
+            })
+        end,
+    },
+}
