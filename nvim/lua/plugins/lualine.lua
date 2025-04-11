a87ba804374605b26c0f5a866c896590c89dcd6d
advaypakhale <advay.pakhale@gmail.com>
2025-04-12 02:51:56 +0800

    Add transparency to neovim

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,6 +2,12 @@ return {
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {},
+        config = function()
+            require("lualine").setup {
+                options = {
+                    theme = "tokyonight",
+                },
+            }
+        end,
     },
 }
--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -3,7 +3,13 @@ return {
         "folke/tokyonight.nvim",
         priority = 10000,
         config = function()
-            require("tokyonight").setup {}
+            require("tokyonight").setup {
+                transparent = true,
+                styles = {
+                    sidebars = "transparent",
+                    floats = "transparent",
+                },
+            }
 
             -- Load the colorscheme here.
             -- Like many other themes, this one has different styles, and you could load
