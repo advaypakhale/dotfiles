caf9ad890d17a4b1d4e4ab52b1fe8670629cecb0
advaypakhale <advay.pakhale@gmail.com>
2025-06-08 01:42:58 +0800

    feat: setup guess-indent.nvim

---
+++ nvim/lua/plugins/guess-indent.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
+        opts = {
+            on_tab_options = {
+                ["expandtab"] = true, -- always expand tabs
+            },
+        },
+    },
+}
