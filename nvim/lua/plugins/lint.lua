912129352bb29cc44ad0a181b04e288deb666558
advaypakhale <advay.pakhale@gmail.com>
2025-06-07 23:53:29 +0800

    fix: remove typo

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -53,7 +53,7 @@ return {
                     group = lint_augroup,
                     callback = function()
                         -- Only run the linter in buffers that you can modify in order to
-                        -- avoid superfluous noise, notably within the handy LSP pop-ups thatAdd commentMore actions
+                        -- avoid superfluous noise, notably within the handy LSP pop-ups that
                         -- describe the hovered symbol using Markdown.
                         if vim.opt_local.modifiable:get() then
                             lint.try_lint()
