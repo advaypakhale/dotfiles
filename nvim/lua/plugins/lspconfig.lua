a9492f725788917503ee3c24a77b3c1966f5e014
advaypakhale <advay.pakhale@gmail.com>
2025-06-08 00:02:46 +0800

    fix: regression

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -138,6 +138,22 @@ return {
                         "goto [d]eclaration"
                     )
 
+                    -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+                    ---@param client vim.lsp.Client
+                    ---@param method vim.lsp.protocol.Method
+                    ---@param bufnr? integer some lsp support methods only in specific files
+                    ---@return boolean
+                    local function client_supports_method(client, method, bufnr)
+                        if vim.fn.has "nvim-0.11" == 1 then
+                            return client:supports_method(method, bufnr)
+                        else
+                            return client.supports_method(
+                                method,
+                                { bufnr = bufnr }
+                            )
+                        end
+                    end
+
                     -- The following two autocommands are used to highlight references of the
                     -- word under your cursor when your cursor rests there for a little while.
                     --    See `:help CursorHold` for information about when this is executed
@@ -147,8 +163,10 @@ return {
                         vim.lsp.get_client_by_id(event.data.client_id)
                     if
                         client
-                        and client:supports_method(
-                            vim.lsp.protocol.Methods.textDocument_documentHighlight
+                        and client_supports_method(
+                            client,
+                            vim.lsp.protocol.Methods.textDocument_documentHighlight,
+                            event.buf
                         )
                     then
                         local highlight_augroup = vim.api.nvim_create_augroup(
@@ -194,8 +212,10 @@ return {
                     -- This may be unwanted, since they displace some of your code
                     if
                         client
-                        and client:supports_method(
-                            vim.lsp.protocol.Methods.textDocument_inlayHint
+                        and client_supports_method(
+                            client,
+                            vim.lsp.protocol.Methods.textDocument_inlayHint,
+                            event.buf
                         )
                     then
                         map("<leader>th", function()
